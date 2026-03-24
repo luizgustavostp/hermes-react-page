9282c0a4c3c2c3b889213b607a414e499dbe986b
Luiz Gustavo <luizgustavosantospreira@gmail.com>
2026-03-24 03:36:08 -0300

    update final?

--- src/components/Avaliation.tsx
+++ src/components/Avaliation.tsx
@@ -8,7 +8,7 @@ interface AvProps {
 export default function Avaliation(props: AvProps) {
     return <div className='Avaliation'>
         <span>
-            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#1c274c"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#fcfcfc" stroke-width="1.488"> <circle cx="12" cy="6" r="4" fill="#1C274C"></circle> <path d="M20 17.5C20 19.9853 20 22 12 22C4 22 4 19.9853 4 17.5C4 15.0147 7.58172 13 12 13C16.4183 13 20 15.0147 20 17.5Z" fill="#1C274C"></path> </g><g id="SVGRepo_iconCarrier"> <circle cx="12" cy="6" r="4" fill="#1C274C"></circle> <path d="M20 17.5C20 19.9853 20 22 12 22C4 22 4 19.9853 4 17.5C4 15.0147 7.58172 13 12 13C16.4183 13 20 15.0147 20 17.5Z" fill="#1C274C"></path> </g></svg>
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#fcfcfc" stroke-width="1.488"> <circle cx="12" cy="6" r="4" fill="#ffffff"></circle> <path d="M20 17.5C20 19.9853 20 22 12 22C4 22 4 19.9853 4 17.5C4 15.0147 7.58172 13 12 13C16.4183 13 20 15.0147 20 17.5Z" fill="#ffffff"></path> </g><g id="SVGRepo_iconCarrier"> <circle cx="12" cy="6" r="4" fill="#ffffff"></circle> <path d="M20 17.5C20 19.9853 20 22 12 22C4 22 4 19.9853 4 17.5C4 15.0147 7.58172 13 12 13C16.4183 13 20 15.0147 20 17.5Z" fill="#ffffff"></path> </g></svg>
             <h1>{props.name}</h1>
         </span>
         <p>{props.text}</p>
